c64ddea76d17df498d52b5d90bf5e9f09a67ff6a
SigismundSchlomo <o.kuznietsov@inc4.net>
2024-05-29 13:52:17 +0300

    airdao.0

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { BigNumber } from '@ethersproject/bignumber'
 
-export const PERMIT2_ADDRESS = '0x000000000022D473030F116dDEE9F6B43aC78BA3'
+export const PERMIT2_ADDRESS = '0x2B75bF9B8ec5966832c123bc24Db3a283E3C1be4'
 
 export const MaxUint48 = BigNumber.from('0xffffffffffff')
 export const MaxUint160 = BigNumber.from('0xffffffffffffffffffffffffffffffffffffffff')
